4989aa72f735f1660a70f1ea65ed19d0a399a3c4
malcogb <158741203+malcogb@users.noreply.github.com>
2025-05-20 18:15:33 +0100

    modification de train.py et demo.py

--- demo.py
+++ demo.py
@@ -6,13 +6,13 @@ from model import BertClassifier
 model_name = "bert-base-multilingual-cased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = BertClassifier(model_name, num_labels=3)
-model.load_state_dict(torch.load("C:/Users/MSI/Desktop/Deep Learning_2/bert-entailment-classifier/train_6.csv"))
+model.load_state_dict(torch.load("C:/Users/MSI/Desktop/Deep Learning_2/bert-entailment-classifier/saved_model.pt"))
 model.eval()
 
 labels_map = {0: "Entailment", 1: "Neutral", 2: "Contradiction"}
 
 def predict(premise, hypothesis):
-    inputs = tokenizer(premise, hypothesis, return_tensors="pt", padding=True, truncation=True, max_length=128)
+    inputs = tokenizer(premise, hypothesis, return_tensors="pt", padding=True, truncation=True, max_length=259) # c'est le max_length du dataset utilisé (train_6.csv)
     with torch.no_grad():
         outputs = model(**inputs)
         pred = torch.argmax(outputs, dim=1).item()
--- train.py
+++ train.py
@@ -6,15 +6,15 @@ from torch import nn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import wandb
-from model import BertClassifier  # Assure-toi que model.py est dans le même dossier
+from model import BertClassifier  # Il faut que model.py est dans le même dossier
 
 # --- Config ---
 config = {
     "model_name": "bert-base-multilingual-cased",
-    "max_length": 128,
+    "max_length": 259, # c'est le max_length du dataset utilisé (train_6.csv)
     "batch_size": 16,
     "lr": 2e-5,
-    "epochs": 20,
+    "epochs": 10,
     "num_labels": 3,
     "device": "cuda" if torch.cuda.is_available() else "cpu"
 }
@@ -118,7 +118,7 @@ def main():
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
     loss_fn = nn.CrossEntropyLoss()
 
-    best_val_acc = 0
+    best_val_acc = 0.80
     for epoch in range(cfg.epochs):
         train_loss, train_acc = train_epoch(model, train_loader, optimizer, loss_fn, cfg.device)
         val_loss, val_acc = eval_epoch(model, val_loader, loss_fn, cfg.device)
@@ -137,11 +137,16 @@ def main():
               f"Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}")
 
         # Sauvegarde du meilleur modèle
+        torch.save(model.state_dict(), "C:/Users/MSI/Desktop/Deep Learning_2/bert-entailment-classifier/saved_model.pt") 
         if val_acc > best_val_acc:
             best_val_acc = val_acc
-            torch.save(model.state_dict(), "C:/Users/MSI/Desktop/Deep Learning_2/bert-entailment-classifier/best_model.pt") 
-            print(f"Meilleur modèle sauvegardé avec accuracy {best_val_acc:.4f}")
+            print(f"Le modèle sauvegardé a dépassé le seuil de performance souhaité \n"
+            f"et présente une val_accuracy de : {best_val_acc:.4f}")
 
+        else:
+          print(f"Le modèle sauvegardé n'a pas atteint le seuil de performance de val_accuracy souhaité qui vaut {best_val_acc:.4f}. \n"
+            f"Il présente une val_accuracy de : {val_acc:.4f}")
+          
     wandb.finish()
 
 if __name__ == "__main__":
